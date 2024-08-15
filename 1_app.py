a5955b0d267550e804bace81df2d67f1eb893b5c
rafaellanes <rafael.lanes@primum.edu.br>
2024-08-15 13:49:33 -0300

    feat: primeiro data frame do streamlit

--- 1_app.py
+++ 1_app.py
@@ -1,11 +1,42 @@
 import os
 from dotenv import load_dotenv
+import psycopg2
+import pandas as pd
+import streamlit as st
 
 load_dotenv()
 
-DB_USER = os.getenv('DB_USER')
+DB_USER     = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_TABLE    = os.getenv('DB_TABLE') 
+DB_HOST     = os.getenv('DB_HOST')
 
-print(DB_PASSWORD)
+def conectando_bd():
+# Conectando ao banco de dados
+    conn = psycopg2.connect(
+        dbname=DB_TABLE,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST
+    )
 
+    # Criando um cursor
 
+    return conn
+
+connect_bd = conectando_bd()
+
+query = """
+    select
+        "SK_TEMPO_DATA_PAGAMENTO"
+        ,"TIPO_VENDA"
+        ,SUM("RECEITA_CONTRATADA")
+    from receita_contratada
+    where "SK_TEMPO_DATA_PAGAMENTO" >= '2024-08-01'
+    group by "SK_TEMPO_DATA_PAGAMENTO","TIPO_VENDA"
+    order by "SK_TEMPO_DATA_PAGAMENTO" ASC
+"""
+
+df_vendas = pd.read_sql_query(query,connect_bd)
+
+st.dataframe(df_vendas)
